9ba7e3e2d73e6dc9939345e30f04f00dc7c51890
jgranados4 <jgranados@est.ups.edu.ec>
2024-07-13 12:33:55 -0500

    `chore: actualizar componente de notificación con estilos y lógica de tipos`

--- src/app/shared/components/notificacion/notificacion.component.ts
+++ src/app/shared/components/notificacion/notificacion.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { UtilidadService } from '@core/services/utilidad.service';
 
 @Component({
   selector: 'app-notificacion',
   standalone: true,
   imports: [],
   templateUrl: './notificacion.component.html',
-  styleUrl: './notificacion.component.css'
+  styleUrl: './notificacion.component.css',
 })
 export class NotificacionComponent {
-
+  private utilidad = inject(UtilidadService);
+  mensaje = this.utilidad.mensaje;
+  tipos = this.utilidad.tipos;
 }
