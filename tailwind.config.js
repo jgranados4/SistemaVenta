24f458aecc82f5c3f82e407672644e4f3c5422ec
jgranados4 <jgranados@est.ups.edu.ec>
2024-07-13 12:34:05 -0500

    `chore: actualizar dependencia de sweetalert2 a la versión 11.12.2`

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
+    colors: {
+      blueC: "#1d4ed8",
+    },
     extend: {},
   },
   plugins: [require("flowbite/plugin")],
