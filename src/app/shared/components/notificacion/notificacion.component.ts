2d5384122d314a3ff7c457924f34924c7a6288a8
jgranados4 <jgranados@est.ups.edu.ec>
2025-04-25 23:06:53 -0500

    Característica: mejora en la gestión de notificaciones y actualización de la tabla con soporte para modales de edición de usuario y producto

--- src/app/shared/components/notificacion/notificacion.component.ts
+++ src/app/shared/components/notificacion/notificacion.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { UtilidadService } from '@core/services/utilidad.service';
 
 @Component({
--- src/app/shared/components/table-rtze/table-rtze.component.ts
+++ src/app/shared/components/table-rtze/table-rtze.component.ts
@@ -3,36 +3,47 @@ import {
   EventEmitter,
   Input,
   input,
+  OnInit,
+  output,
   Output,
+  OutputEmitterRef,
   signal,
   Signal,
   WritableSignal,
 } from '@angular/core';
+import { ModalProductoComponent } from '@component/layout/modales/modal-producto/modal-producto.component';
 import { ModalUsuarioComponent } from '@component/layout/modales/modal-usuario/modal-usuario.component';
+import { Producto } from '@core/models/producto';
 import { Accion } from '@core/models/table/tabla-columna';
 import { Usuario } from '@core/models/usuario';
 
 @Component({
   selector: 'app-table-rtze',
   standalone: true,
-  imports: [ModalUsuarioComponent],
+  imports: [ModalUsuarioComponent, ModalProductoComponent],
   templateUrl: './table-rtze.component.html',
   styleUrl: './table-rtze.component.css',
+  host: {
+    class: 'overflow-x-auto shadow-md sm:rounded-lg',
+  },
 })
-export class TableRtzeComponent {
+export class TableRtzeComponent implements OnInit {
   // titulo = input<string>('');
   title = '';
   columnas: any[] = [];
   dataSource: any = [];
-  //
+  showUserModal: boolean = false;
+  showProductModal: boolean = false;
+  editarT = signal<string>('Editar Producto');
   //
   idData = signal<Usuario | undefined>(undefined);
+  idDataPro = signal<Producto | undefined>(undefined);
   editar = signal<string>('Editar');
   modalSwitch = signal<boolean>(false);
 
-  @Input() set titulo(title: any) {
-    this.title = title;
-  }
+  ngOnInit(): void {}
+  titulo = input<string>('');
+  onEliminarProduct = output<Producto>();
 
   @Input() set columns(columns: any[]) {
     this.columnas = columns;
@@ -40,16 +51,26 @@ export class TableRtzeComponent {
   @Input() set data(data: any) {
     this.dataSource = data;
   }
-  @Output() action: EventEmitter<Accion> = new EventEmitter();
+
+  // action = output<Accion>();
+  // @Output() action: EventEmitter<Accion> = new EventEmitter();
   onAction(accion: string, row?: any) {
-    if (accion == 'Editar') {
+    if (accion === 'Editar Usuario') {
+      this.showUserModal = true;
+      this.showProductModal = false;
       this.idData.set(row);
       this.openModal();
     }
-    this.action.emit({
-      accion: accion,
-      fila: row,
-    });
+    if (accion === 'Editar Producto') {
+      this.idDataPro.set(row);
+      this.openModal();
+      this.showProductModal = true;
+      this.showUserModal = false;
+    }
+  }
+  eliminar(producto: Producto) {
+    console.log('Eliminar');
+    this.onEliminarProduct.emit(producto);
   }
   openModal() {
     this.modalSwitch.set(!this.modalSwitch());
