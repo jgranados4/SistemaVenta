4428fdae4e768e51f423c331047ab0966e466be3
jgranados4 <jgranados@est.ups.edu.ec>
2025-05-14 18:55:07 -0500

    Característica: mejora en la gestión de datos y eliminación de usuarios con alertas de éxito

--- src/app/shared/components/table-rtze/table-rtze.component.ts
+++ src/app/shared/components/table-rtze/table-rtze.component.ts
@@ -1,8 +1,13 @@
 import {
+  ChangeDetectionStrategy,
   Component,
+  computed,
+  effect,
   EventEmitter,
+  inject,
   Input,
   input,
+  model,
   OnInit,
   output,
   Output,
@@ -16,6 +21,8 @@ import { ModalUsuarioComponent } from '@component/layout/modales/modal-usuario/m
 import { Producto } from '@core/models/producto';
 import { Accion } from '@core/models/table/tabla-columna';
 import { Usuario } from '@core/models/usuario';
+import { showAlert } from '@core/models/utility.Alert';
+import { UsuarioStoreService } from '@core/services/SignalStore/usuario-store.service';
 
 @Component({
   selector: 'app-table-rtze',
@@ -23,54 +30,79 @@ import { Usuario } from '@core/models/usuario';
   imports: [ModalUsuarioComponent, ModalProductoComponent],
   templateUrl: './table-rtze.component.html',
   styleUrl: './table-rtze.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     class: 'overflow-x-auto shadow-md sm:rounded-lg',
   },
 })
-export class TableRtzeComponent implements OnInit {
-  // titulo = input<string>('');
-  title = '';
+export class TableRtzeComponent {
   columnas: any[] = [];
-  dataSource: any = [];
-  showUserModal: boolean = false;
-  showProductModal: boolean = false;
   editarT = signal<string>('Editar Producto');
+  editar = signal<string>('Editar');
   //
   idData = signal<Usuario | undefined>(undefined);
   idDataPro = signal<Producto | undefined>(undefined);
-  editar = signal<string>('Editar');
+
   modalSwitch = signal<boolean>(false);
+  showUserModalSwitch = signal<boolean>(false);
+  showProductModalSwitch = signal<boolean>(false);
 
-  ngOnInit(): void {}
+  UsuarioStor = inject(UsuarioStoreService);
+
   titulo = input<string>('');
-  onEliminarProduct = output<Producto>();
-
+  TipoEntidad = computed(() => {
+    const valor = this.titulo();
+    if (valor == 'EditarUsuario') return 'EditarUsuario';
+    if (valor == 'EditarProducto') return 'EditarProducto';
+    return 'otro';
+  });
+  //
   @Input() set columns(columns: any[]) {
     this.columnas = columns;
   }
-  @Input() set data(data: any) {
-    this.dataSource = data;
-  }
-
-  // action = output<Accion>();
-  // @Output() action: EventEmitter<Accion> = new EventEmitter();
+  data2 = model<any[]>([]);
+  data = computed(() => {
+    return this.data2();
+  });
+  public efectos = effect(() => {
+    console.log('efeto', this.TipoEntidad());
+    console.log('efecto de data model', this.data());
+    // Este efecto se ejecutará cuando idData cambie.
+    if (this.idData()) {
+      console.log('ID de Usuario actualizado:', this.idData());
+    }
+    if (this.idDataPro()) {
+      console.log('ID de Producto actualizado:', this.idDataPro());
+    }
+  });
   onAction(accion: string, row?: any) {
     if (accion === 'Editar Usuario') {
-      this.showUserModal = true;
-      this.showProductModal = false;
+      this.showUserModalSwitch.set(true);
+      this.showProductModalSwitch.set(false);
       this.idData.set(row);
       this.openModal();
     }
     if (accion === 'Editar Producto') {
       this.idDataPro.set(row);
+      this.showProductModalSwitch.set(true);
+      this.showUserModalSwitch.set(false);
       this.openModal();
-      this.showProductModal = true;
-      this.showUserModal = false;
     }
   }
-  eliminar(producto: Producto) {
-    console.log('Eliminar');
-    this.onEliminarProduct.emit(producto);
+  eliminar(accion: string, row?: any) {
+    this.UsuarioStor.eliminar(row).subscribe({
+      next: () => {
+        console.log('Usuario eliminado correctamente');
+        showAlert(
+          '¡Operación exitosa!',
+          'La Eliminado se completó correctamente.',
+          'success'
+        );
+      },
+      error: (err) => {
+        console.error('Error al eliminar el usuario:', err);
+      },
+    });
   }
   openModal() {
     this.modalSwitch.set(!this.modalSwitch());
