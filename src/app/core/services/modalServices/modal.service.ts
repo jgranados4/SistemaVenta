3eb5d66f5f00f15adc2a946d180df3db1c394c0f
jgranados4 <jgranados@est.ups.edu.ec>
2025-11-22 20:41:26 -0500

    Feat: Añadir servicio modal para gestionar la apertura y cierre de diálogos, mejorando la reutilización y la experiencia de usuario en la aplicación.

---
+++ src/app/core/services/modalServices/modal.service.ts
@@ -0,0 +1,35 @@
+import { ComponentType } from '@angular/cdk/overlay';
+import { inject, Injectable } from '@angular/core';
+import { MatDialog, MatDialogConfig} from '@angular/material/dialog';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ModalService {
+  private readonly dialog = inject(MatDialog);
+
+  openModal<CT, D = any>(
+    componentRef: ComponentType<CT>,
+    config?: {
+      data?: D;
+      isEditing?: boolean;
+      width?: string;
+      disableClose?: boolean;
+      panelClass?: string | string[];
+    }
+  ): void {
+    const dialogConfig: MatDialogConfig = {
+      width: config?.width ?? '500px',
+      disableClose: config?.disableClose ?? false,
+      panelClass: config?.panelClass,
+      data: {
+        data: config?.data,
+        isEditing: config?.isEditing ?? false,
+      },
+    };
+    this.dialog.open(componentRef, dialogConfig);
+  }
+  closeModal(): void {
+    this.dialog.closeAll();
+  }
+}
