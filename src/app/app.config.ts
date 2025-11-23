c3ca68b2f3ae65bb9d9696e9940fdf1baaafe9fb
jgranados4 <jgranados@est.ups.edu.ec>
2025-11-22 20:45:17 -0500

    Feat: Añadir soporte para manejo de errores global y detección de cambios sin zona en la configuración de la aplicación, mejorando la robustez y el rendimiento.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 import { errorInterceptor } from '@core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch(),withInterceptors([errorInterceptor]))],
+  providers: [
+    provideRouter(routes),
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideHttpClient(withFetch(), withInterceptors([errorInterceptor])),
+  ],
 };
