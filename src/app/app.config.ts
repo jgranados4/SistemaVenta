22142ac60336a5c0eb1150146988f954f0b1ed74
jgranados4 <jgranados@est.ups.edu.ec>
2025-12-21 13:57:06 -0500

    Refactor: Agregar configuración de formatos de fecha globales y ajustar la inicialización del tema en app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,7 @@
 import {
   ApplicationConfig,
+  inject,
+  provideAppInitializer,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
@@ -12,12 +14,30 @@ import {
   withInterceptors,
 } from '@angular/common/http';
 import { errorInterceptor } from '@core/interceptors/error.interceptor';
-
+import { ThemeService } from '@core/services';
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
+export const GLOBAL_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'dd/MM/yyyy', // Formato visual (Angular DatePipe syntax)
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideHttpClient(withFetch(), withInterceptors([errorInterceptor])),
+    provideAppInitializer(()=>{
+      const themeService = inject(ThemeService);
+      themeService.init();
+    }),
+    { provide: MAT_DATE_LOCALE, useValue: 'es-EC' },
+    provideNativeDateAdapter()
   ],
 };
